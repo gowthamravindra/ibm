31d476f42b3d5bd54712e22bd2b3c9f152f3210e
Harish <Harish@DESKTOP-3I1IS3K>
2018-01-12 15:26:04 +0530

    adding

---
+++ mavenjekins/src/main/java/mavenjekins/test.java
@@ -0,0 +1,10 @@
+package mavenjekins;
+
+public class test {
+	
+	public void sample() {
+		System.out.println("helloworld");
+	}
+	}
+
+
---
+++ mavenjekins/src/test/java/mavenjekins/exe.java
@@ -0,0 +1,14 @@
+package mavenjekins;
+
+import org.testng.annotations.Test;
+
+public class exe extends test{
+	
+	@Test(priority=1)
+		
+		public void vy(){
+			sample();
+		}
+	}
+
+
